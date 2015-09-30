f1d9292853a5bf07adad9e3ca74846604e93e652
amercader <amercadero@gmail.com>
2015-09-30 14:55:26 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:2.7
+
+RUN mkdir /srv/app
+
+WORKDIR /src/app
+
+COPY app.py /srv/app/app.py
+COPY requirements.txt /srv/app/requirements.txt
+
+RUN pip install -r /srv/app/requirements.txt
+
+EXPOSE 8000
+CMD python /srv/app/app.py
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    if request.remote_addr:
+
+        url = 'http://freegeoip.net/json/{0}'.format(request.remote_addr)
+        r = requests.get(url).json()
+        return "Hi, it looks like you are around {0}.".format(r['city'])
+    else:
+        return "Hi, we don't know where you are."
+
+if __name__ == "__main__":
+    app.run(port=8000)
